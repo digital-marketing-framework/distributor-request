f6006c768837dae0454026582e5afd10be3493d2
Stephan Ude <ude@mediatis.de>
2025-05-20 09:54:17 +0200

    MFR-136 shorten error messages based on status code

--- src/DataDispatcher/RequestDataDispatcher.php
+++ src/DataDispatcher/RequestDataDispatcher.php
@@ -196,7 +196,7 @@ class RequestDataDispatcher extends DataDispatcher implements RequestDataDispatc
     {
         $statusCode = $response->getStatusCode();
         if ($statusCode < 200 || $statusCode >= 400) {
-            throw new DigitalMarketingFrameworkException('Response status code indicates an error: ' . $statusCode);
+            throw new DigitalMarketingFrameworkException('Status code: ' . $statusCode);
         }
     }
 
@@ -213,7 +213,7 @@ class RequestDataDispatcher extends DataDispatcher implements RequestDataDispatc
             $response = $client->request($this->method, $this->url, $requestOptions);
             $this->checkResponse($response);
         } catch (GuzzleException $e) {
-            throw new DigitalMarketingFrameworkException($e->getMessage(), $e->getCode(), $e);
+            throw new DigitalMarketingFrameworkException('Status code: ' . $e->getCode(), $e->getCode(), $e);
         }
     }
 
